d97aabeb29b35114000d358041d12d14ebdb22dc
Keyeric <keyeric.bristol@gmail.com>
2020-03-03 11:58:01 -0700

    added some components for routing

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import PrivateRoute from "./components/PrivateRoute";
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <Nav />
         <Switch>
-          <PrivateRoute exact path="/protected" component={} />
+          <PrivateRoute exact path="/protected" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route component={Login} />
         </Switch>
