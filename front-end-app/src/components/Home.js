a08b7b80732477c0c746ac21011cee97fcb6f121
Keyeric <keyeric.bristol@gmail.com>
2020-03-03 16:33:05 -0700

    just pushing the last bit of crud for now

--- front-end-app/src/components/Home.js
+++ front-end-app/src/components/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Nav from "./Nav";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  home: {},
+  nav: {
+    backgroundColor: "black",
+    "& a": {
+      margin: "0 2rem",
+      backgroundColor: "grey",
+      textDecoration: "none",
+      color: "white"
+    }
+  }
+}));
 
 function Home() {
+  const classes = useStyles();
   return (
-    <div>
-      <Nav />
+    <div className={classes.home}>
+      <div className={classes.nav}>
+        <Nav />
+      </div>
       <h2> This is the home page</h2>
     </div>
   );
--- front-end-app/src/components/Nav.js
+++ front-end-app/src/components/Nav.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  nav: {
+    display: "flex"
+  }
+}));
 
 function Nav() {
+  const classes = useStyles();
   return (
-    <nav>
-      <ul>
-        <a href="./protected"> Home </a>
-        <a href="./search"> Search </a>
-        {/* Might make into overlay component? */}
-        <a href="./about"> About </a>
-        {/* Link to Jonah's about page */}
-      </ul>
+    <nav classname={classes.nav}>
+      <a href="./protected"> Home </a>
+      <a href="./search"> Search </a>
+      {/* Might make into overlay component? */}
+      <a href="./about"> About </a>
+      {/* Link to Jonah's about page */}
     </nav>
   );
 }
