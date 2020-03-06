a22c51485b865bb2a54c66f9a831e429a207aa18
Keyeric <keyeric.bristol@gmail.com>
2020-03-05 18:13:18 -0700

    Changed start screen to sign up since YOU NEED TO DO THAT

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <Switch>
           {/* <Route exact path="/" component={Home} /> */}
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={SignUp} />
           <PrivateRoute exact path="/protected" component={HomeKey} />
           <Route path="/Home" component={HomeKey} />
           <Route path="/Login" component={Login} />
